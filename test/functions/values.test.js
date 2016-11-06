bc85e13ecacdc465ea187bdaccdd7b30dd8e87c3
Matt Glover <matthewglover@me.com>
2016-11-06 09:02:47 +0000

    test(functions): add tests for functions

---
+++ test/functions/values.test.js
@@ -0,0 +1,25 @@
+import test from 'ava';
+import { values } from '../../src/functions';
+
+const o = {
+  a: 1,
+  b: '2',
+  c: [1, 2, 3],
+  d: { a: 1, b: 2, c: 3 },
+  e: false,
+  f: null,
+};
+
+
+test('values :: {String: *} -> [*]', (t) => {
+  t.deepEqual(
+    values(o),
+    [
+      1,
+      '2',
+      [1, 2, 3],
+      { a: 1, b: 2, c: 3 },
+      false,
+      null,
+    ]);
+});
